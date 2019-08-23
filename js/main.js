b9e4291d06792a0df6eede98fa28e81419bd56d2
Karin Duboy <karinduboy@gmail.com>
2019-08-22 21:47:11 -0300

    avances cnon el fetch, aun me da problemas

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const apiKey = "c6f4b7af00ff89712efe89669fe19897";
 var baseUrl, moviesByCategory, apiConf, totalItems;
 var category = ''
-const categories = ['popular','top_rated','upcoming','now_playing']
+const categoryList = ['popular','top_rated','upcoming','now_playing']
 
 // traemos la configuracion necesaria para usar las url de la API
 async function getApiConf (){
@@ -15,7 +15,7 @@ async function getApiConf (){
 async function getMovieResults (category) {
     const response = await fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
     const movieResults = await response.json()
-    return movieResults
+    return await movieResults
 }
 
 const setConfVars = () => {
@@ -28,37 +28,44 @@ const setConfVars = () => {
 
 //con esto debemos poder crear los items de la home...la idea es llamar a esta función en el onload de la home
 const setHomeMovieItems = (categoryList) => {
-    // debugger;
-    categoryList.forEach( category = () => {
+    categoryList.forEach( category => {        
         let container = document.getElementById(`${category}Results`)
+        container.innerHTML=''
         let sectionTL = document.createElement('div');
         sectionTL.classList.add('sectionTopLine')
         let sectionTitle = document.createElement('h2');
         sectionTitle.classList.add('sectionTitle')
         sectionTitle.classList.add('topLine')
+        sectionTitle.innerText = category
         let viewAllLink = document.createElement('a');
         viewAllLink.classList.add('viewAllLink')
         viewAllLink.classList.add('topLine')
         viewAllLink.href = '#'
         viewAllLink.innerText = 'View All...'
-        moviesByCategory = getMovieResults(category)
-            .then (moviesByCategory.results.slice(0,5).forEach( movieItem = () => {
-                console.log(movieItem)
-                // let titleContainer = document.createElement('div');
-                // let titlePosterContainer = document.createElement('div')
-                // let titlePoster = document.createElement('img')
-                // let titleName = document.createElement('p')
-                // titleContainer.classList.add('titleContainer')
-                // titlePosterContainer.classList.add('titlePoster')
-                // titlePoster.classList.add('titlePoster')
-                // titleName.classList.add('titleName')
+        sectionTL.appendChild(sectionTitle)
+        sectionTL.appendChild(viewAllLink)
+        container.appendChild(sectionTL)
+        getMovieResults(category)
+            .then ( movieResults => movieResults.results.slice(0,5).forEach( movieItem => {
+                let titleContainer = document.createElement('div');
+                let titlePosterContainer = document.createElement('div');
+                let titlePoster = document.createElement('img');
+                let titleName = document.createElement('p');
+                titleContainer.classList.add('titleContainer')
+                titlePosterContainer.classList.add('titlePoster')
+                titlePoster.classList.add('titlePoster')
+                titleName.classList.add('titleName')
+                
                 // titleContainer.id = movieItem.id
                 // titlePoster.src = `${apiConf.base_url}/w500/${movieItem.poster_url}`
                 // titleName.innerText = movieItem.name
                 // titlePosterContainer.appendChild(titlePoster)
                 // titlePosterContainer.appendChild(titleName)
                 // titleContainer.appendChild(titlePosterContainer)
-                // container.appendChild(titleContainer)
+                // return titleContainer
             }))
-    })
-}
+        container.appendChild(titleContainer)
+    })}
+    // }
+        
+
