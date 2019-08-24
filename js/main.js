cc39b1e9f6bcd688003a532b29ad58eef2476eff
Karin Duboy <karinduboy@gmail.com>
2019-08-24 17:15:34 -0300

    correcciones de async/await pero aún no logro traerme los movies de cada categoría

--- js/main.js
+++ js/main.js
@@ -30,43 +30,37 @@ const createElement = (tag,elemClasses,elementId='',text='') => {
     return element
 }
 
-// const setChilds = (father,[childList]) => {
-//     debugger;
-//     childList.forEach( child => father.appendChild(child))
-// }
+const setChilds = (father, childList) => {
+    childList.forEach( (child) => {father.appendChild(child)})
+}
 
 const setNode = (nodeId) => {
     let container = document.getElementById(nodeId)
     return container
 }
 
-const setHomeMovieItems = (categoryList) => {
-    let container = document.getElementById('homeResultsContainer');
+const setHomeMovieItems = async (categoryList) => {
+    let container = setNode('homeResultsContainer')
     container.innerHTML='';
-    categoryList.forEach( async (category) => {
+    categoryList.forEach(async (category) => {
+        let movies = await getCategoryMovieResults(category);
         let categoryContainer = createElement('section',['sectionContainer'],`${category}Results`);
         let categoryMoviesContainer = createElement('div',['firstTitles'])
         let topLineContainer = createElement ('div',['sectionTopLine'])
-        let sectionTitle = createElement('p',['sectionTitle','topLine'],category)
-        let viewAllLink = createElement('a',['viewAllLink','topLine'],'View All...')
-        // setChilds(topLineContainer,[sectionTitle,viewAllLink])
-        // setChilds(categoryContainer,[categoryMoviesContainer])
-        topLineContainer.appendChild(sectionTitle)
-        topLineContainer.appendChild(viewAllLink)
-        categoryContainer.appendChild(topLineContainer)
-        categoryContainer.appendChild(categoryMoviesContainer)
-        moviesByCategory = await getCategoryMovieResults(category)
-        // console.log(moviesByCategory)
-        moviesByCategory.slice(0,5).forEach( movie => {
-            let movieItem = createElement('a',['titleContainer'],movie);
-            let movieImg = createElement('img',['titlePoster']);
-            movieImg.src = `${apiConf.images.base_url}/w342/${movie.poster_path}`
-            let movieName = createElement('p',['titleName'],movie.title);
-            movieItem.appendChild(movieImg)
-            movieItem.appendChild(movieName)
-            categoryMoviesContainer.appendChild(movieItem)
-        })
-        categoryContainer.appendChild(categoryMoviesContainer)
+        let sectionTitle = createElement('p',['sectionTitle','topLine'],'',category)
+        let viewAllLink = createElement('a',['viewAllLink','topLine'],'','View All...')
+        setChilds(topLineContainer,[sectionTitle,viewAllLink])
+        setChilds(categoryContainer,[topLineContainer,categoryMoviesContainer])
+        console.log(movies) //para comprobar si en algún momento trae el resultado de las pelis pero tira undefined siempre, por eso no agarra el foreach
+        // movies.slice(0, 5).forEach( (movie) => {
+        //     let movieItem = createElement('a',[ 'titleContainer' ],movie);
+        //     let movieImg = createElement('img',[ 'titlePoster' ]);
+        //     movieImg.src = `${apiConf.images.base_url}/w342/${movie.poster_path}`;
+        //     let movieName = createElement('p',[ 'titleName' ],'',movie.title);
+        //     setChilds(movieItem,[movieImg,movieName])
+        //     setChilds(categoryMoviesContainer,[movieItem])
+        // })
+        setChilds(container,[categoryContainer])
     })
 }
 
