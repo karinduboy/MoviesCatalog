2d6e6e7bbe7c44d8526d60c8fb24f65dc5ad3e95
Karin Duboy <karinduboy@gmail.com>
2019-08-24 16:29:31 -0300

    avances de async/await pero aún no espera las promesas, aunque si las ejecuta sin errores de network

--- js/main.js
+++ js/main.js
@@ -1,72 +1,76 @@
 // definimos las variables globales
-const apiKey = "c6f4b7af00ff89712efe89669fe19897";
+var apiKey = "c6f4b7af00ff89712efe89669fe19897";
 var baseUrl, moviesByCategory, apiConf, totalItems, titleContainer, container;
 var category = ''
 const categoryList = ['popular','top_rated','upcoming','now_playing']
+// var 
 
-// traemos la configuracion necesaria para usar las url de la API
-async function getApiConf (){
-    const result = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`);
-    apiConf = await result.json();
-    baseUrl = await apiConf.images.base_url 
-    return await baseUrl
+
+const getApiConf = () => {
+    fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`)
+    .then ( res => res.json())
+        .then( res => apiConf = res )
 }
 
-//traemos la lista de las peliculas y sus detalles indicando una categoría como parámetro
-async function getMovieResults (category) {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
-    const movieResults = await response.json()
-    return await movieResults
+const getCategoryMovieResults = (category) => {
+    fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
+    .then ( res => res.json())
+        .then ( res => moviesByCategory = res.results)
+}
+// const setConfVars = async(apiConf) = {
+//     baseUrl = await apiConf.images.base_url
+//     return baseUrl
+// }
+
+const createElement = (tag,elemClasses,elementId='',text='') => {
+    let element = document.createElement(tag);
+    elemClasses.forEach( eClass => element.classList.add(eClass))
+    element.id = elementId
+    element.innerText = text
+    return element
 }
 
-const setConfVars = () => {
-    getApiConf()
-    .then( res = () => {
-        baseUrl = res.base_url
-        return baseUrl
-    })
+// const setChilds = (father,[childList]) => {
+//     debugger;
+//     childList.forEach( child => father.appendChild(child))
+// }
+
+const setNode = (nodeId) => {
+    let container = document.getElementById(nodeId)
+    return container
 }
 
-//con esto debemos poder crear los items de la home...la idea es llamar a esta función en el onload de la home
 const setHomeMovieItems = (categoryList) => {
-    categoryList.forEach( category => {        
-        getMovieResults(category)
-            .then (
-                container = document.getElementById(`${category}Results`);
-                container.innerHTML=''
-                let sectionTL = document.createElement('div');
-                sectionTL.classList.add('sectionTopLine')
-                let sectionTitle = document.createElement('h2');
-                sectionTitle.classList.add('sectionTitle')
-                sectionTitle.classList.add('topLine')
-                sectionTitle.innerText = category
-                let viewAllLink = document.createElement('a');
-                viewAllLink.classList.add('viewAllLink')
-                viewAllLink.classList.add('topLine')
-                viewAllLink.href = '#'
-                viewAllLink.innerText = 'View All...'
-                sectionTL.appendChild(sectionTitle)
-                sectionTL.appendChild(viewAllLink)
-                container.appendChild(sectionTL)
-                movieResults => movieResults.results.slice(0,5).forEach( movieItem => {
-                    titleContainer = document.createElement('div');
-                    let titlePosterContainer = document.createElement('div');
-                    let titlePoster = document.createElement('img');
-                    let titleName = document.createElement('p');
-                    titleContainer.classList.add('titleContainer')
-                    titlePosterContainer.classList.add('titlePoster')
-                    titlePoster.classList.add('titlePoster')
-                    titleName.classList.add('titleName')
-                    titleContainer.id = movieItem.id
-                    titlePoster.src = `${baseUrl}/w500/${movieItem.poster_path}`
-                    titleName.innerText = movieItem.title
-                    titlePosterContainer.appendChild(titlePoster)
-                    titlePosterContainer.appndChild(titleName)
-                    titleContainer.appendChild(titlePosterContainer)
-                    container.appendChild(titleContainer)
-                })
-            )
+    let container = document.getElementById('homeResultsContainer');
+    container.innerHTML='';
+    categoryList.forEach( async (category) => {
+        let categoryContainer = createElement('section',['sectionContainer'],`${category}Results`);
+        let categoryMoviesContainer = createElement('div',['firstTitles'])
+        let topLineContainer = createElement ('div',['sectionTopLine'])
+        let sectionTitle = createElement('p',['sectionTitle','topLine'],category)
+        let viewAllLink = createElement('a',['viewAllLink','topLine'],'View All...')
+        // setChilds(topLineContainer,[sectionTitle,viewAllLink])
+        // setChilds(categoryContainer,[categoryMoviesContainer])
+        topLineContainer.appendChild(sectionTitle)
+        topLineContainer.appendChild(viewAllLink)
+        categoryContainer.appendChild(topLineContainer)
+        categoryContainer.appendChild(categoryMoviesContainer)
+        moviesByCategory = await getCategoryMovieResults(category)
+        // console.log(moviesByCategory)
+        moviesByCategory.slice(0,5).forEach( movie => {
+            let movieItem = createElement('a',['titleContainer'],movie);
+            let movieImg = createElement('img',['titlePoster']);
+            movieImg.src = `${apiConf.images.base_url}/w342/${movie.poster_path}`
+            let movieName = createElement('p',['titleName'],movie.title);
+            movieItem.appendChild(movieImg)
+            movieItem.appendChild(movieName)
+            categoryMoviesContainer.appendChild(movieItem)
+        })
+        categoryContainer.appendChild(categoryMoviesContainer)
     })
 }
-        
 
+const initialize = async() => {
+    await getApiConf();
+    setHomeMovieItems(categoryList)
+}
